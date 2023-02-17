0777838ae9e63e5584ce1512d3e59bf4729cd735
Albert Osei Frimpong <60059672+Alotab@users.noreply.github.com>
2023-02-17 22:25:01 +0000

    adding logic functions

--- mylib/logic.py
+++ mylib/logic.py
@@ -5,3 +5,10 @@ def wiki(name="war Goddess", length=1):
     """This is a wikipedia fetcher"""
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
+
+
+
+def search_wiki(name):
+    """ search wikipedia for names"""
+    results = wikipedia.search(name)
+    return results
